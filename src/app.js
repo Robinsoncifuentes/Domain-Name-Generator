062303d302386e3ae0e4d82499a573344404c621
Robinsoncifuentes <robinsonmunoz4@gmail.com>
2022-02-09 03:05:18 +0000

    nombre de dominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our", "her"];
+let adj = ["great", "big", "small"];
+let noun = ["jogger", "racoon", "dog"];
+let dominio = [".com", ".cl", ".net"];
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let g = 0; g < noun.length; g++) {
+      for (let k = 0; k < dominio.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[g] + dominio[k]);
+      }
+    }
+  }
+}
